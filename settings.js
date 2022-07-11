d5a5f4a4cf5424ab9fa41145db0d324611c94ab1
hackerz-lab <98409596+hackerz-lab@users.noreply.github.com>
2022-07-11 20:51:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': '4c6e693182e3', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'4c6e693182e3', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
